6c1dd18e7d1258cb5fce45e484be365919ea4460
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-11-22 03:41:56 +0200

    Game logic finished (without total timer)

--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,147 @@
-// SLAVE
+// MASTER
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <Servo.h>
 #include <SPI.h>
 
-#define BTN_PL1 2
-#define BTN_PL2 3
-#define R1 4
-#define G1 5
-#define B1 6
-#define R2 7
-#define G2 8
-#define B2 9
+#define RS 4
+#define EN 5
+#define D4 6
+#define D5 7
+#define D6 8
+#define D7 9
+
+#define SERVO_PIN 3
 
 #define DUMMY 255
 #define PL1_BTN_SIG 10
 #define PL2_BTN_SIG 20
+#define TIMEOUT_SIG 128
 
-#define R_MIN 910
-#define R_MAX 970
-#define G_MIN 830
-#define G_MAX 890
-#define B_MIN 680
-#define B_MAX 740
+#define PLAYER_BITMASK 0b00011111 
 
-volatile byte receivedByte, responseByte;
+String player1="", player2="";
 
-void player1_ISR();
-void player2_ISR();
-void setRGB(int player, int color);
-void ledCycle(byte c1, byte c2);
-byte mapColor(int analogData);
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+Servo motor;
 
-ISR(SPI_STC_vect) {
-    Serial.print("Slave received value: ");
-    receivedByte = SPDR;
-    Serial.print(receivedByte);
-    Serial.println("");
-    if(receivedByte >= 0 && receivedByte <= 15) {
-        byte col1 = receivedByte & 3;
-        byte col2 = (receivedByte >> 2) & 3;
-        ledCycle(col1, col2);
-    }
-}
+int pl1_pts = 0, pl2_pts = 0;
+
+bool gameStarted = false;
+
+unsigned long timeout = 2000;
+
+byte sendByte(byte data);
+void initialState();
+void printPts();
+void playRound();
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(SCK, INPUT);
-    pinMode(MOSI, INPUT);
-    pinMode(MISO, OUTPUT);
-    pinMode(SS, INPUT);
-    SPI.attachInterrupt();
-    SPCR |= _BV(SPE);
-    attachInterrupt(digitalPinToInterrupt(BTN_PL1), player1_ISR, RISING);
-    attachInterrupt(digitalPinToInterrupt(BTN_PL2), player2_ISR, RISING);
-    for(int i=R1; i<=B2; i++) {
-        pinMode(i, OUTPUT);
-    }
+  Serial.begin(9600);
+
+  SPI.begin();
+  pinMode(SS, OUTPUT);
+  digitalWrite(SS, HIGH);
+
+  lcd.begin(16, 2);
+
+  motor.attach(SERVO_PIN);
+  motor.write(0);
+
+  randomSeed(analogRead(0));
 }
+
+byte l = 0;
 
 void loop() {
-    // Serial.println("SLAVE ALIVE");
-    // setRGB(1, value) where value was sent by the master
+    initialState();
+    while(gameStarted) {
+        printPts();
+        playRound();
+        delay(500);
+    }
 }
 
-void player1_ISR() {
-    int readVal = analogRead(A0);
-    Serial.print("Player 1: ");
-    Serial.print(readVal);
+byte sendByte(byte data) {
+  digitalWrite(SS, LOW);
+  byte slaveResponse = SPI.transfer(data);
+  digitalWrite(SS, HIGH);
+  if(data != DUMMY) {
+    Serial.print("Sent byte to slave: ");
+    Serial.print(data);
     Serial.println("");
-    responseByte = PL1_BTN_SIG;
-    byte colorCode = mapColor(readVal);
-    responseByte |= (colorCode << 5);
-    SPDR = responseByte;
+  }
+  
+  return slaveResponse;
 }
 
-void player2_ISR() {
-    int readVal = analogRead(A1);
-    Serial.print("Player 2: ");
-    Serial.print(readVal);
+void initialState() {
+  lcd.home();
+  lcd.print("Press any button");
+  lcd.setCursor(0, 1);
+  lcd.print("to begin.");
+  byte receivedFromSlave = sendByte(DUMMY);
+  if ((receivedFromSlave & PLAYER_BITMASK) == PL1_BTN_SIG || (receivedFromSlave & PLAYER_BITMASK) == PL2_BTN_SIG) {
+    gameStarted = true;
+    sendByte(DUMMY);
+  }
+}
+
+void printPts() {
+  lcd.clear();
+  lcd.home();
+  lcd.print("Player 1: "); lcd.print(pl1_pts); lcd.print(" pts");
+  lcd.setCursor(0, 1);
+  lcd.print("Player 2: "); lcd.print(pl2_pts); lcd.print(" pts");
+}
+
+void playRound() {
+  byte col1 = random(1, 4);
+  byte col2 = random(1, 4);
+  byte colsToSend = col1 | (col2 << 2);
+  byte slaveResponse = sendByte(colsToSend);
+  Serial.println("COLOR SENT TO SLAVE");
+  unsigned long startTime = millis();
+
+  do {
+    slaveResponse = sendByte(DUMMY); 
+    Serial.print("Slave response: ");
+    Serial.print(slaveResponse);
     Serial.println("");
-    responseByte = PL2_BTN_SIG;
-    byte colorCode = mapColor(readVal);
-    responseByte |= (colorCode << 5);
-    SPDR = responseByte;
-}
+  }
+ while ((slaveResponse == DUMMY || !(slaveResponse & 0b01100000)) && millis() - startTime < timeout);
 
-void setRGB(int player, int color) {
-    int r, g, b;
-    if(player == 1) {
-        r = 4; g = 5; b = 6;
-    } else if(player == 2) {
-        r = 7; g = 8; b = 9;
+  if (millis() - startTime >= timeout) {
+    sendByte(TIMEOUT_SIG);
+    Serial.println("TIMEOUT SIGNAL SENT!");
+  } else {
+    byte color = (slaveResponse & 0b01100000) >> 5;
+    slaveResponse = slaveResponse & PLAYER_BITMASK;
+    if(slaveResponse == PL1_BTN_SIG) {
+      if(color == col1) {
+        Serial.println("Player 1 pressed the correct button");
+        pl1_pts += (millis() - startTime)/100;
+      } else {
+        Serial.println("Player 1 pressed the wrong button");
+        pl1_pts -= 20;
+        if(pl1_pts < 0) pl1_pts = 0;
+      }
+      sendByte(TIMEOUT_SIG);
+    } else if(slaveResponse == PL2_BTN_SIG) {
+      if(color == col2) {
+        Serial.println("Player 2 pressed the correct button");
+        pl2_pts += (millis() - startTime)/100;
+      } else {
+        Serial.println("Player 2 pressed the wrong button");
+        pl2_pts -= 20;
+        if(pl2_pts < 0) pl2_pts = 0;
+      }
+      sendByte(TIMEOUT_SIG);
     } else {
-        Serial.println("Player select error");
-        return;
+      Serial.println("An error occured! The player bitmask is incorrect.");
+      // Serial.print("Slave response: ");
+      // Serial.print(slaveResponse);
+      // Serial.println("");
     }
-    Serial.print("Player ");
-    Serial.print(player);
-    Serial.println("");
-    Serial.print("Color ");
-    Serial.print(color);
-    Serial.println("");
-    Serial.println("");
-    switch(color) {
-        case 0:
-            digitalWrite(r, LOW);
-            digitalWrite(g, LOW);
-            digitalWrite(b, LOW);
-            break;
-        case 1:
-            digitalWrite(r, HIGH);
-            digitalWrite(g, LOW);
-            digitalWrite(b, LOW);
-            break;
-        case 2:
-            digitalWrite(r, LOW);
-            digitalWrite(g, HIGH);
-            digitalWrite(b, LOW);
-            break;
-        case 3:
-            digitalWrite(r, LOW);
-            digitalWrite(g, LOW);
-            digitalWrite(b, HIGH);
-            break;
-        case 4:
-            digitalWrite(r, HIGH);
-            digitalWrite(g, HIGH);
-            digitalWrite(b, HIGH);
-            break;
-        default:
-            Serial.println("Color select error.");
-            // digitalWrite(r, HIGH);
-            // digitalWrite(g, LOW);
-            // digitalWrite(b, HIGH);
-            return;
-    }
-}
-
-void ledCycle(byte c1, byte c2) {
-    setRGB(1, c1);
-    setRGB(1, c2);
-}
-
-byte mapColor(int analogData) {
-    if(analogData >= R_MIN && analogData <= R_MAX) return 1;
-    else if(analogData >= G_MIN && analogData <= G_MAX) return 2;
-    else if(analogData >= B_MIN && analogData <= B_MAX) return 3;
-    else return 0;
+  }
 }
